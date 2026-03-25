65a4cd592b77414ff00b8416b67d95472ddbe50c
Mohammad Alrashed <139542347+DatariusAI@users.noreply.github.com>
2026-03-25 11:28:23 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
 
+
 router.get('/', homeController.getHome);
+
+
+router.post('/chat', (req, res) => {
+  const { message } = req.body;
+
+  if (!message) {
+    return res.status(400).json({
+      error: 'Message is required'
+    });
+  }
+
+  res.json({
+    reply: `You said: ${message}`
+  });
+});
 
 module.exports = router;
